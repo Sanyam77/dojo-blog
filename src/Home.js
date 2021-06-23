1823f44584b6764a2805285034fd7974a9388895
Sanyam77 <techtube771@gmail.com>
2021-06-23 23:57:24 +0530

    added blog-list

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Lorem ipsum dolor sit adipisicing ",
+      body: "Reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Lorem amet consectetur adipisicing ",
+      body: "Elit. Accusamus odio reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Lorem adipisicing ",
+      body: "Accusamus odio reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className="home">
-      <h1>HomePage</h1>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
