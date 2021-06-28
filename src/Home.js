c6bf7b264c019edf9e3a3ecb1949dce00d8cd257
Sanyam77 <techtube771@gmail.com>
2021-06-28 16:47:57 +0530

    initial

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Lorem ipsum dolor sit adipisicing ",
-      body: "Reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Lorem amet consectetur adipisicing ",
-      body: "Elit. Accusamus odio reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
-      author: "yoshi",
-      id: 2,
-    },
-    {
-      title: "Lorem adipisicing ",
-      body: "Accusamus odio reprehenderit amet repudiandae quidem, esse temporibus blanditiis ullam error repellat vero nesciunt optio laudantium rerum, enim vel minima aliquid tenetur?",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
+  // this empty [] is necessary, but why?
+  // this ensures that useEffect runs only at start and not every time blogs change , to prevent inf loop
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's Blogs"
-        handleDelete={handleDelete}
-      />
+      {isPending && <div>Loading...</div>}
+      {blogs && (
+        // <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        <BlogList blogs={blogs} title="All Blogs" />
+      )}
+      {/* for using js in template, we need {} */}
+      {/* blogs && is req, else when the blog is null initially, we get error in mapping(see bloglist.js). React evaluates left first, if flase then doesn't bother about right. When blogs is updated with some value, it renders again and the right is rendered this time */}
     </div>
   );
 };
